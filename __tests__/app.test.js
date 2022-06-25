646ce91fa12127ba58e944cf0db33d67f76b51d0
THOMAS HARDING <hardingt604@gmail.com>
2022-06-24 18:38:14 -0600

    fix expected redirect uri in tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -18,7 +18,7 @@ describe('gitty routes', () => {
     const res = await request(app).get('/api/v1/github/login');
 
     expect(res.header.location).toMatch(
-      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
+      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=https:\/\/thom-backend-gitty.herokuapp.com\/api\/v1\/github\/callback/i
     );
   });
 
